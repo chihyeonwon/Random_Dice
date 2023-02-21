cc43a3215952c12f4f4935b8f1111ec821ee4ae3
ylose <heungeob1003@gmail.com>
2023-02-21 14:03:20 +0900

    RootScreen 스펠링 수정

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class RootSreen extends StatelessWidget {
-  const RootSreen({Key? key}) : super(key: key);
+class RootScreen extends StatelessWidget {
+  const RootScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
