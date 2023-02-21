85377c6a2fe27c21b7bc8c585aaf4a873f92ddc0
ylose <heungeob1003@gmail.com>
2023-02-21 14:11:49 +0900

    TabBarView 작업

--- lib/screen/root_screen.dart
+++ lib/screen/root_screen.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
+  // TickerProviderStateMaxin 사용하기
+
+  TabController? controller; // 사용할 TabController 선언
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TabController(length: 2, vsync: this); // 컨트롤러 초기화하기
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:TabBarView( // 탭 화면을 보여줄 위젯
-        children:renderChildren(),
+      body: TabBarView(
+        // 탭 화면을 보여줄 위젯
+        controller: controller, // 컨트롤러 등록하기
+        children: renderChildren(),
       ),
-
       bottomNavigationBar: renderBottomNavigation(),
     );
   }
 }
-
 
 List<Widget> renderChildren() {
   return [];
@@ -22,5 +38,5 @@ List<Widget> renderChildren() {
 
 BottomNavigationBar renderBottomNavigation() {
   // 탭 내비게이션을 구현하는 위젯
-  return BottomNavigationBar(items:[]);
+  return BottomNavigationBar(items: []);
 }
