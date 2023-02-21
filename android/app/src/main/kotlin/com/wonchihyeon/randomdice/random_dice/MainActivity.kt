dab272915f20da2d8208264756a9e6d8ea7f3587
ylose <heungeob1003@gmail.com>
2023-02-21 13:25:57 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/wonchihyeon/randomdice/random_dice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wonchihyeon.randomdice.random_dice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:random_dice/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:HomeScreen(),
+    )
+  )
+}
+
+
