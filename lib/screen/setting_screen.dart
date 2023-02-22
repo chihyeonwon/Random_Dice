de17c267d52c27ad8bd4cb44afba200d97cb3cd3
ylose <heungeob1003@gmail.com>
2023-02-22 12:49:01 +0900

    두 번째 인덱스 화면에 민감도 조절 슬라이더 추가

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_dice/const/colors.dart';
 
 class SettingScreen extends StatelessWidget {
   final double threshold; // Slider 민감도의 현재값
@@ -12,6 +13,33 @@ class SettingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding:const EdgeInsets.only(left:20.0),
+          child:Row(
+            children:[
+              Text(
+                '민감도',
+                style:TextStyle(
+                  color:primaryColor,
+                  fontSize:20.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Slider(
+          min:0.1, // 최솟값
+          max:10.0, // 최댓값
+          divisions:101, // 최솟값과 최댓값 사이 구간 개수
+          value:threshold, // 슬라이더 선택값
+          onChanged:onThresholdChange, // 값 변경 시 실행되는 함수
+          label: threshold.toStringAsFixed(1), // 표싯값
+        ),
+      ],
+    );
   }
 }
